b7959967a0fb08e1120e0f09698467df4a8e7655
Akash Kumar Shrivastva <akash@influenxers.com>
2025-03-04 23:34:31 -0500

    extract visual features

--- app/config/settings.py
+++ app/config/settings.py
@@ -9,10 +9,11 @@ class Config:
     LLM_API_KEY = os.getenv('CLAUDE_API_KEY')
     LLM_API_URL = "https://api.anthropic.com/v1/messages"
     MODEL_NAME = "claude-3-haiku-20240307"
+    # MODEL_NAME = "claude-3-5-sonnet-20240620"
     MAX_TOKENS = 1500
 
     # Video processing settings
-    MIN_SCENE_CHANGE_THRESHOLD = 20.0
+    MIN_SCENE_CHANGE_THRESHOLD = 15.0
     MIN_INTERVAL_SECONDS = 1.0
 
     # Audio processing settings
--- app/routes/analytics_routes.py
+++ app/routes/analytics_routes.py
@@ -1,11 +1,13 @@
 import os
+from typing import List
 
 from flask import Blueprint, request, jsonify
 
-from app.services.video_service import VideoService
+from app.models.video import Video
+from app.services.recommendation_service import RecommendationService
 
 bp = Blueprint('video', __name__)
-video_analytics_service = VideoService()
+recommendation_service = RecommendationService()
 
 
 @bp.route('/analyze_video', methods=['POST'])
@@ -24,12 +26,36 @@ def analyze_video():
             return jsonify({'error': 'Video file not found at specified path'}), 404
 
         # Process the video directly from the path
-        summary, screenplay = video_analytics_service.process_video(video_path, caption)
+        summary, screenplay = recommendation_service.process_video(video_path, caption)
 
         return jsonify({
             'summary': summary,
             'screenplay': screenplay['screenplay'],
         })
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@bp.route('/suggest_edits', methods=['POST'])
+def suggest_edits():
+    try:
+        # get high performing videos
+        data = request.json
+
+        high_performing_videos: List[Video] = [Video(**item) for item in data.get('high_performing', [])]
+        low_performing_video = Video(**data.get('low_performing'))
+
+        """
+        In real use case, we need to:
+        1. extract the features of the low performing video
+        2. get the features of the high performing videos using similarity search
+        2. then perform comparison
+        """
+
+        edits = recommendation_service.suggest_edits(high_performing_videos, low_performing_video)
+
+        return jsonify(edits)
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
